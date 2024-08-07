463f735294f599e5997475ea0e8b82a16b6f32bc
Free Style <thecodingskill@gmail.com>
2024-08-07 17:15:12 +0700

    add new url

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ app.get('/', function(req, res){
 
 
 
+
+app.get('/v1', function(req, res){
+	res.send("Hello APP1");
+});
+
 app.listen(8080,function(){
 	console.log('app is running in port 8080');
 })
