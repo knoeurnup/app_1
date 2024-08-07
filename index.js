0986ceaed87529c32b3f4005d86fb87af063e1d7
Free Style <thecodingskill@gmail.com>
2024-08-07 17:31:59 +0700

    new version test tage

--- index.js
+++ index.js
@@ -9,8 +9,13 @@ app.get('/', function(req, res){
 
 
 app.get('/v1', function(req, res){
-	res.send("Hello APP1");
+	res.send("Hello V2");
 });
+
+app.get('/v2', function(req, res){
+	res.send("Hello V2");
+});
+
 
 app.listen(8080,function(){
 	console.log('app is running in port 8080');
